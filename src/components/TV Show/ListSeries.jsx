99d3a833c7b6bbdb116276b1c759484eed19038d
ekoprassetyo <ekop2331@@gmail.com>
2022-09-26 20:58:08 +0700

    Update style card series

--- src/components/TV Show/ListSeries.jsx
+++ src/components/TV Show/ListSeries.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Square from "../../components/Timbul.module.css"
+import Square from '../../components/Timbul.module.css'
 import {Link} from 'react-router-dom'
 
 function ListSeries({id, seriesImg, title, year}) {
   return (
     <Link to={`/video/${id}`} className='text-decoration-none'>
     <Card className='rounded border-0 bg-black text-white' style ={{backgroundColor: "black"}}>
-      <Card.Img variant="top" src={movieImg} className={Square.Square}/>
+      <Card.Img variant="top" src={seriesImg} className={Square.Square}/>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <p className='text-muted'>{year}</p>
