8fce18c8e51170c3cf5c5070c04eeb1c07820442
ekoprassetyo <ekop2331@@gmail.com>
2022-09-26 11:50:44 +0700

    Deploy frontend

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -20,7 +20,7 @@ function PaymentHome() {
     //change this to the script source you want to load, for example this is snap.js sandbox env
     const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
     //change this according to your client-key
-    const myMidtransClientKey = "SB-Mid-client-aDYXJFh6Atm_jUfl";
+    const myMidtransClientKey = process.env.REACT_APP_MIDTRANS_CLIENT_KEY;
   
     let scriptTag = document.createElement("script");
     scriptTag.src = midtransScriptUrl;
