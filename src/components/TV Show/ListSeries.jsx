1fcb6f8ceea5f6d00ef2de0e628157bbf6288457
ekoprassetyo <ekop2331@@gmail.com>
2022-09-26 21:00:05 +0700

    Update style card series

--- src/components/TV Show/ListSeries.jsx
+++ src/components/TV Show/ListSeries.jsx
@@ -7,7 +7,7 @@ function ListSeries({id, seriesImg, title, year}) {
   return (
     <Link to={`/video/${id}`} className='text-decoration-none'>
     <Card className='rounded border-0 bg-black text-white' style ={{backgroundColor: "black"}}>
-      <Card.Img variant="top" src={seriesImg} className={Square.Square}/>
+     <a href="/video"><Card.Img variant="top" src={seriesImg} className={Square.Square}/></a>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <p className='text-muted'>{year}</p>
